f286232132dcda5a0c9b2d3329125e004c0f2865
Chandu J S <app.coder@outlook.in>
2019-12-10 15:46:27 +0400

    basic swift code

--- ios/Classes/SwiftFlutterStickersPlugin.swift
+++ ios/Classes/SwiftFlutterStickersPlugin.swift
@@ -2,14 +2,36 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterStickersPlugin: NSObject, FlutterPlugin {
+  private static let DefaultBundleIdentifier: String = "WA.WAStickersThirdParty"
+  private static let WhatsAppURL: URL = URL(string: "whatsapp://stickerPack")!
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_stickers", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterStickersPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    print(call.method)
-    result("iOS " + UIDevice.current.systemVersion)
+    let pasteboard: UIPasteboard = UIPasteboard.general
+    let jsonData: [String: Any] = call.arguments as! [String: Any]
+    let PasteboardStickerPackDataType: String = Bundle.main.bundleIdentifier!
+    
+    let dataToSend: Data = try! JSONSerialization.data(withJSONObject: jsonData, options: [])
+    
+    if #available(iOS 10.0, *) {
+      pasteboard.setItems([[PasteboardStickerPackDataType: dataToSend]], options: [UIPasteboardOption.localOnly: true, UIPasteboardOption.expirationDate: NSDate(timeIntervalSinceNow: 60)])
+    } else {
+      pasteboard.setData(dataToSend, forPasteboardType: PasteboardStickerPackDataType)
+    }
+    
+    DispatchQueue.main.async {
+      if #available(iOS 10.0, *) {
+          UIApplication.shared.open(SwiftFlutterStickersPlugin.WhatsAppURL, options: [:], completionHandler: nil)
+      } else {
+          UIApplication.shared.openURL(SwiftFlutterStickersPlugin.WhatsAppURL)
+      }
+    }
+    
+    result(true)
   }
 }
