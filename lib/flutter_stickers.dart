384e2dce90abcbd3102accfe19f77b4f555c7670
Chandu J S <app.coder@outlook.in>
2019-12-11 14:21:33 +0400

    flutter based intent data sharing try

--- lib/flutter_stickers.dart
+++ lib/flutter_stickers.dart
@@ -1,12 +1,26 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:intent/intent.dart';
 
 class FlutterStickers {
   static const MethodChannel _channel =
       const MethodChannel('flutter_stickers');
 
   static Future<void> callMethod(Map<String, dynamic> data) async {
-    await _channel.invokeMethod('callMethod', data);
+    if (Platform.isIOS) {
+      await _channel.invokeMethod('callMethod', data);
+    }
+    if (Platform.isAndroid) {
+      final Intent intent = Intent();
+
+      intent.setAction("com.whatsapp.intent.action.ENABLE_STICKER_PACK");
+      intent.putExtra("sticker_pack_id", '1');
+      intent.putExtra("sticker_pack_authority", 'authority');
+      intent.putExtra("sticker_pack_name", 'Sticker Pack Name');
+
+      intent.startActivity();
+    }
   }
 }
